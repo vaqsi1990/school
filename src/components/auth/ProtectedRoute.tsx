691dbe5f284512c2f287e71eca3c6de8947765e0
vaqsi1990 <vaqsii23@gmail.com>
2025-08-29 12:41:38 +0400

    &apos

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -51,7 +51,7 @@ export default function ProtectedRoute({
       <div className="min-h-screen flex items-center justify-center">
         <div className="text-center">
           <h1 className="text-2xl font-bold text-red-600 mb-4">Access Denied</h1>
-          <p className="text-gray-600">You don't have permission to view this page.</p>
+          <p className="text-gray-600">You don&apos;t have permission to view this page.</p>
         </div>
       </div>
     )
